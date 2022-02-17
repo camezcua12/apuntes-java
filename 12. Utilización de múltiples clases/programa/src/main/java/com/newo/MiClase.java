8f42df0246e28ee8f7008c8c6f3b31129afa8b41
Carlos Amezcua <carlosamezcuapuertas@gmail.com>
2022-02-17 12:58:25 +0000

    12. Utilización de múltiples clases

---
+++ 12. Utilización de múltiples clases/programa/src/main/java/com/newo/MiClase.java
@@ -0,0 +1,17 @@
+package com.newo;
+
+public class MiClase {
+    public static String cadena = "alicia";
+
+    public static void metodoHolaMundo() {
+        System.out.println("Hola mundo");
+    }
+
+    public static void metodoAdiosMundo() {
+        System.out.println("Adiós mundo");
+    }
+
+    public static void enviarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
---
+++ 12. Utilización de múltiples clases/programa/src/main/java/com/newo/OtraClase.java
@@ -0,0 +1,7 @@
+package com.newo;
+
+public class OtraClase {
+    public String obtenerMensaje() {
+        return "Mi nombre es Carlos.";
+    }
+}
